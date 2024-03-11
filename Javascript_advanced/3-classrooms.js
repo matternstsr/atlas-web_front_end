6470e7c54fac470c00ff8118696fa69f3f4d11d5
matternstsr <matternstsr@gmail.com>
2024-03-11 10:02:27 -0500

    3

---
+++ Javascript_advanced/3-classrooms.js
@@ -0,0 +1,13 @@
+3. Closure and loops
+// Write a function named createClassRoom:
+// It takes into argument numbersOfStudents (number)
+// Inside, it contains a function studentSeat, that takes into argument seat (number) and returns a function that returns the seat number
+// After the definition of studentSeat, create and populate a variable students (array)
+// Using a loop from 0 to numbersOfStudents, pass the number of iteration + 1 to studentSeat and add its return value to the students array
+// Returns the students array
+// Create a closure classRoom, calling createClassRoom with 10 students
+// Requirements:
+// Executing the following code:
+// console.log(classRoom[0]());
+// console.log(classRoom[3]());
+// console.log(classRoom[9]());
