e3eacbca17d8c45efc137a62aab3da7e5b045016
matternstsr <matternstsr@gmail.com>
2024-03-11 12:09:34 -0500

    7 works

---
+++ Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,60 @@
+// 6. Private methods with closure
+// Write a module named studentHogwarts:
+// It contains two variables privateScore set to 0, and name set to null
+// It contains one private method changeScoreBy, it takes points in argument and add it to privateScore
+// The module gives access to four public methods (return an object):
+// setName, it takes into argument newName, and set the private variable name
+// rewardStudent, it calls the method changeScoreBy with 1
+// penalizeStudent, it calls the method changeScoreBy with -1
+// getScore, it returns name: score (ex: Harry: 14)
+// Create one variable named harry, that is an instance of studentHogwarts:
+// Set the name of the object to Harry
+// Reward the student four times
+// Log to the console the name and score
+// Create one variable named draco, that is an instance of studentHogwarts:
+// Sets the name of the object to Draco
+// Reward the student one time
+// Penalize the student three times
+// Log to the console the name and score
+// Requirements:
+// Executing the code should display in the console
+// Harry: 4
+// Draco: -2
+
+const studentHogwarts = () => {
+  let privateScore = 0;
+  let name = null;
+  const changeScoreBy = points => {
+      privateScore += points;
+  }
+  return {
+      setName(newName) {
+          name = newName;
+      },
+      rewardStudent() {
+          changeScoreBy(1);
+      },
+      penalizeStudent() {
+          changeScoreBy(-1);
+      },
+      getScore() {
+          return `${name}: ${privateScore}`;
+      }
+  };
+};
+
+let harry = studentHogwarts();
+harry.setName('Harry');
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+let draco = studentHogwarts();
+draco.setName('Draco');
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+console.log(draco.getScore());
