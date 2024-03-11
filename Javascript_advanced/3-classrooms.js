1f32a4056317429fb86f0381fb814b3649f108f4
matternstsr <matternstsr@gmail.com>
2024-03-11 10:10:22 -0500

    hmm

--- Javascript_advanced/3-classrooms.js
+++ Javascript_advanced/3-classrooms.js
@@ -20,7 +20,7 @@ const createClassRoom = (numbersOfStudents) => {
       }
   }
   let students = []
-  for (let i=0; i <= numbersOfStudents; i++) {
+  for (let i = 0; i <= numbersOfStudents; i++) {
       students.push(studentSeat(i + 1))
   }
   return students
