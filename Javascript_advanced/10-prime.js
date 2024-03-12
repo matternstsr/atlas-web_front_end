ed65af3b9ccc668a407a31cfefbb683cf67fa9e0
matternstsr <matternstsr@gmail.com>
2024-03-12 12:32:26 -0500

    this is 10

--- Javascript_advanced/10-prime.js
+++ Javascript_advanced/10-prime.js
@@ -5,24 +5,40 @@
 // Requirements:
 // Your code should display something in the range of Execution time of calculating prime numbers 100 times was 40.865000002551824 milliseconds.
 
-const calculating = () => {
+const countPrimeNumbers = () => {
   let primeCount = 0;
 
   for (let number = 2; number <= 100; number++) {
       let isPrime = true;
       for (let divisor = 2; divisor <= Math.sqrt(number); divisor++) {
-          if (number % divisor === 0) {isPrime = false; break;}}
+          if (number % divisor === 0) {
+              isPrime = false;
+              break;
+          }
+      }
       if (isPrime) {
           primeCount++;
       }
   }
   return primeCount;
 };
+
 const measureRunTime = (func, times) => {
   const startTime = performance.now();
-  for (let i = 0; i < times; i++) {
-      func();}
-  const endTime = performance.now();
-  console.log(`Execution time of ${func.name} prime numbers ${times} times was ${endTime - startTime} milliseconds.`);
+  let count = 0;
+
+  const runFunction = () => {
+      func();
+      count++;
+      if (count < times) {
+          setTimeout(runFunction, 0);
+      } else {
+          const endTime = performance.now();
+          console.log(`Execution time of caclulating prime numbers ${times} times was ${endTime - startTime} milliseconds.`);
+      }
+  };
+
+  runFunction();
 };
-measureRunTime(calculating, 100);
+
+measureRunTime(countPrimeNumbers, 100);
