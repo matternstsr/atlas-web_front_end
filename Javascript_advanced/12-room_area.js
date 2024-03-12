5a5860472dd086150dc05a63c0b390763d5660bd
matternstsr <matternstsr@gmail.com>
2024-03-12 14:11:47 -0500

    working with area

--- Javascript_advanced/12-room_area.js
+++ Javascript_advanced/12-room_area.js
@@ -5,3 +5,14 @@
 // getArea: function that returns the surface area of a the object using the width and length
 // Create a variable named boundGetArea, that will bind the object roomDimensions to the getArea function
 
+const roomDimensions = {
+  width: 50,
+  length: 100,
+  getArea: function() {
+    return this.width * this.length;
+  }
+};
+
+const boundGetArea = roomDimensions.getArea.bind(roomDimensions);
+
+console.log(boundGetArea());
